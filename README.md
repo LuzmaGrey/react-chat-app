d276a6ab78ffaf7e922ad0f998f9b8aff7fb1c44
LuzmaGrey <67597231+LuzmaGrey@users.noreply.github.com>
2022-10-06 14:21:35 -0500

    project uploaded

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chat app
+
+### [Example Website](https://bespoke-buttercream-0cda87.netlify.app/)
+
+## Introduction
+
+This is litle chat app built with React JS and [Chat Engine](https://chatengine.io)!
+
+## Users and Password to test it
+(user 1) test2react
+(password) 123123
+
+(user 2) test1react
+(password) 123123
+
+## Want to build this too?
+
+To deploy the program we first install yarn and then run it from npm start
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'f9443364-4688-443f-b8a8-090f9516bfc9';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
